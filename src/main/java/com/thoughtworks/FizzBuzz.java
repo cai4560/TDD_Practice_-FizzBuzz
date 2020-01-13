56a6f33e9a689d42b7bc6b24d71142df4c3092b4
Luo jie <jluo@thoughtworks.com>
2020-01-13 15:43:09 +0800

    Refactor: extract RuleCompositor

--- src/main/java/com/thoughtworks/FizzBuzz.java
+++ src/main/java/com/thoughtworks/FizzBuzz.java
@@ -1,17 +1,19 @@
 package com.thoughtworks;
 
-import com.thoughtworks.rule.ContainRule;
-import com.thoughtworks.rule.MultipleRule;
 import com.thoughtworks.rule.Rule;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
 public class FizzBuzz {
+
+    private final RuleCompositor ruleCompositor;
+
+    public FizzBuzz() {
+        ruleCompositor = new RuleCompositor();
+    }
 
     public static void main(String[] args) throws IOException {
         FileWriter writer = new FileWriter("result.txt", false);
@@ -28,35 +30,11 @@ public class FizzBuzz {
     }
 
     public String say(int number) {
-        return initialRules(number).stream()
+        return ruleCompositor.compositeRules(number).stream()
                 .filter(Rule::isEnabled)
                 .map(rule -> rule.apply(number))
                 .flatMap(Optional::stream)
                 .findFirst()
                 .orElse(String.valueOf(number));
-    }
-
-    private List<Rule> initialRules(int number) {
-        boolean contain7 = contains(number, "7");
-        boolean contain5 = !contain7 && contains(number, "5");
-        boolean contain3 = !contain5 && contains(number, "3");
-
-        List<MultipleNumber> multipleNumbers = new ArrayList<>();
-        if (!contain5) {
-            multipleNumbers.add(MultipleNumber.FIZZ);
-        }
-        if (!contain7) {
-            multipleNumbers.add(MultipleNumber.BUZZ);
-        }
-        multipleNumbers.add(MultipleNumber.WHIZZ);
-
-        return List.of(
-                new MultipleRule(!contain3, multipleNumbers),
-                new ContainRule(!contain5, "3", "Fizz")
-        );
-    }
-
-    private boolean contains(int number, String factor) {
-        return String.valueOf(number).contains(factor);
     }
 }
